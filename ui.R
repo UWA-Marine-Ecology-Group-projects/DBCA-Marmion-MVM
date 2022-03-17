6622e1af5a48c767665acb184e3123a6a789d505
Matthew Navarro <00090483@uwa.edu.au>
2022-03-17 15:17:21 +0800

    fix wording on last question

--- ui.R
+++ ui.R
@@ -593,7 +593,7 @@ html, body, .test_map_div {
                                   textAreaInput(
                                     "generalcomment",
                                     width = "94%",
-                                    label = "Please provide any additional comments about values for the marine areas of the South Coast that has not already been covered in previous questions:",
+                                    label = "Please provide any additional comments about values for the marine areas of the Marmion Coast that have not already been covered in previous questions:",
                                     placeholder = NULL,
                                     height = "200px"
                                   ),
