565f6a0ec568aa29f57631500aca399bbe010b53
Brooke Gibbons <brooke.gibbons@uwa.edu.au>
2022-03-22 12:12:43 +0800

    mobile java

--- www/js/mobile.js
+++ www/js/mobile.js
@@ -16,3 +16,9 @@ $.extend(isMobileBinding, {
 });
 
 Shiny.inputBindings.register(isMobileBinding);
+
+
+$(document).on('shiny:sessioninitialized', function (e) {
+  var mobile = window.matchMedia("only screen and (max-width: 768px)").matches;
+  Shiny.onInputChange('is_mobile_device', mobile);
+});
